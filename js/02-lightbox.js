de90b0e77e933688290d2fe7635f59651d76b91c
Andrii Zorba <andrej.zorba@gmail.com>
2022-09-30 16:28:10 +0300

    add 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const onCreateGalleryElevents = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" 
+         alt="${description}"/></a>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", onCreateGalleryElevents);
+
+gallery.addEventListener("click", onClickGallery);
+
+function onClickGallery(event) {
+  event.preventDefault();
+  const lightbox = new SimpleLightbox(".gallery .gallery__link", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
